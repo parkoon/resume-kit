bbf0543c9683a38130523e0a9eb33196175925b6
parkoon <devparkoon@gmail.com>
2020-12-07 19:28:50 +0900

    feat: github page 배포를 위한 prefix 적용

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const withImages = require('next-images')
+const { homepage } = require('./package.json')
+
+const { NODE_ENV } = process.env
+
+module.exports = withImages({
+  assetPrefix: (() => {
+    if (NODE_ENV === 'production' && homepage) {
+      try {
+        console.log('> Detected homepage url in package.json')
+        const { pathname } = new URL(homepage)
+        if (pathname !== '/') {
+          console.log(`> Apply \'${pathname}\' to assetPrefix(subPath)`)
+          return pathname
+        }
+        return ''
+      } catch {
+        console.log('> Can not parse homepage URL not apply assetPrefix(subPath)')
+        return ''
+      }
+    }
+    return ''
+  })(),
+})
