ef0d711f4c6128ca14f058f795947cefe60daa82
parkoon <devparkoon@gmail.com>
2020-12-07 19:39:48 +0900

    chore: sample read json
    getStaticProps 실행되는 시점이 Node 이기 때문에 fs 와 path 모듈을 사용할 수 있다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import path from 'path'
+import fs from 'fs'
 
 function Home(props: any) {
-  return <div>{props.message}</div>
+  return (
+    <div>
+      {props.message} {props.json}
+    </div>
+  )
 }
 
 export async function getStaticProps(context: any) {
+  const jsonPath = path.join(process.cwd(), '/db/sample.json')
+  const jsonData = fs.readFileSync(jsonPath, 'utf-8')
   return {
     props: {
       message: `Hello Next`,
+      json: jsonData,
     },
   }
 }
