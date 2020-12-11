1605344894d23f7098fddf1a359a791834d1671c
parkoon <devparkoon@gmail.com>
2020-12-11 23:18:32 +0900

    feat: reset css

--- src/shared/styles.ts
+++ src/shared/styles.ts
@@ -1,106 +1,66 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  * {
-  user-select: none;
-  -ms-user-select: none;
-  outline: 0;
-  margin: 0;
-  padding: 0;
-}
-
-html {
-  width: 100%;
-  height: 100%;
-  font-size: 10px;
-  font-family: 'Noto Sans KR', sans-serif
-}
-
-#__next {
-  height: 100%;
-  width: 100%;
-}
-
-body {
-  width: 100%;
-  height: 100%;
-}
-
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
+  #__next {
+    height: 100%;
+    width: 100%;
+  }
+  
+  html {
+    width: 100%;
+    height: 100%;
+    font-size: 10px;
+    font-family: 'Noto Sans KR', sans-serif
   }
 
-  *:focus {
-    outline: none;
+  body {
+    width: 100%;
+    height: 100%;
   }
 
-  a,
-  button {
-    color: inherit;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
-  button {
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
     padding: 0;
     border: 0;
-    background: none;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
   }
-
-  input {
-    padding: 0;
-    color: inherit;
-    border-width: 0.1rem;
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
   }
-
-  textarea {
-    padding: 0;
-    border-radius: 0;
-    resize: none;
+  body {
+    line-height: 1;
   }
-
-  input[type='text'],
-  input[type='tel'],
-  input[type='number'],
-  input[type='password'],
-  input[type='email'],
-  input[type='search'],
-  textarea {
-    border-radius: 0;
-    -webkit-appearance: none;
+  ol, ul {
+    list-style: none;
   }
-
-  input[name='password'],
-  input[type='password'] {
-    &::-webkit-credentials-auto-fill-button {
-      visibility: hidden;
-      position: absolute;
-      left: 100%;
-    }
+  blockquote, q {
+    quotes: none;
   }
-
-  input {
-    &::-ms-clear,
-    &::-ms-reveal {
-      display: none;
-      width: 0;
-      height: 0;
-    }
-
-    &::-webkit-search-decoration,
-    &::-webkit-search-cancel-button,
-    &::-webkit-search-results-button,
-    &::-webkit-search-results-decoration {
-      display: none;
-    }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
   }
-
-  img {
-    -webkit-user-drag: none;
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
   }
-
 `
 
 const sizes: { [key: string]: number } = {
