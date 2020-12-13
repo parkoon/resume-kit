0ef0047da77d27d43f347b1fbe3077fc5e240212
parkoon <devparkoon@gmail.com>
2020-12-13 21:45:43 +0900

    feat: complete article page

--- pages/admin/article.tsx
+++ pages/admin/article.tsx
@@ -1,7 +1,87 @@
+import { useState } from 'react'
+import { Button, Modal, List, Typography, Tag, Card } from 'antd'
+
 import AdminLayout from '@Admin/components/AdminLayout'
+import ArticleForm from '@Admin/components/Forms/ArticleForm'
+import { Article } from '@Shared/models/Article'
+import { palette } from '@Shared/styles'
+
+const articles: Article[] = [
+  {
+    title: '퀄슨 (퀄슨)',
+    url: 'zzz',
+    completed: true,
+  },
+  {
+    title: '퀄슨 (퀄슨)',
+    url: 'zzz',
+    completed: true,
+  },
+]
 
 function ArticleManagement() {
-  return <AdminLayout>ArticleManagement</AdminLayout>
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+  return (
+    <AdminLayout
+      pageTitle="내가 작성한 기사들"
+      pageAction={
+        <Button type="primary" onClick={showModal}>
+          만들기
+        </Button>
+      }
+    >
+      <Card>
+        <List
+          dataSource={articles}
+          renderItem={(item: Article) => (
+            <List.Item style={{ justifyContent: 'flex-start' }}>
+              <a
+                href={item.url}
+                target="_blank"
+                style={{ marginRight: 10, color: palette.blue[700] }}
+              >
+                {item.title}
+              </a>
+              {item.completed ? (
+                <Tag color="geekblue">작성완료</Tag>
+              ) : (
+                <Tag color="gold">작성중</Tag>
+              )}
+              <Button type="dashed">수정하기</Button>
+            </List.Item>
+          )}
+        />
+      </Card>
+
+      <Modal
+        title="내가 작성한 기사 🏢"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        bodyStyle={{ maxHeight: '70vh', overflow: 'scroll' }}
+        destroyOnClose
+        footer={[
+          <Button form="article" type="primary" key="submit" htmlType="submit">
+            만들기
+          </Button>,
+        ]}
+      >
+        <ArticleForm id="article" />
+      </Modal>
+    </AdminLayout>
+  )
 }
 
 export default ArticleManagement
