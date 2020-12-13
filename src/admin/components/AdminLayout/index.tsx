1af5aa989650c881ba3dd31b71eb9f8175a7dfa6
parkoon <devparkoon@gmail.com>
2020-12-13 22:37:34 +0900

    refactor: change admin layout
    TODO
    1. 아이템 구조 변경 (아이콘도 넣어야함, 아이콘 찾기)
    2. title, action prop 변경, action은 배열로 받기
    3. 추가로 subtitle 받기

--- src/admin/components/AdminLayout/index.tsx
+++ src/admin/components/AdminLayout/index.tsx
@@ -1,56 +1,65 @@
-import React from 'react'
-import { Layout, Menu, Space, Typography, Button } from 'antd'
+import React, { useState } from 'react'
 import moment from 'moment'
-import { Footer, Header, Content } from 'antd/lib/layout/layout'
+import { Layout, Menu, Button, PageHeader } from 'antd'
 import { useRouter } from 'next/dist/client/router'
+import { TeamOutlined } from '@ant-design/icons'
 
-const { Title } = Typography
+const { Content, Footer, Sider } = Layout
 
+// TODO. 아이템 구조 변경 (아이콘도 넣어야함, 아이콘 찾기)
 const NAV_ITEMS = ['introduce', 'skill', 'project', 'career', 'education', 'etc', 'article']
 
 type AdminLayoutProps = {
   children: React.ReactNode
   center?: boolean
+  // TODO. title, action prop 변경, action은 배열로 받기
+  // TODO. 추가로 subtitle 받기
   pageTitle?: string
   pageAction?: React.ReactNode
 }
 function AdminLayout({ children, center, pageTitle, pageAction }: AdminLayoutProps) {
   const router = useRouter()
+  const [visible, setVisible] = useState(false)
 
-  const centerStyle = center
-    ? { display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }
-    : {}
   return (
-    <Layout style={{ height: '100%' }}>
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[router.pathname.split('/')[2]]}>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={visible} onCollapse={(collapsed) => setVisible(collapsed)}>
+        {/* TODO. LOGO */}
+        <div
+          className="logo"
+          style={{
+            height: '32px',
+            margin: '16px',
+            background: 'rgba(255, 255, 255, 0.3)',
+          }}
+        />
+
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={[router.pathname.split('/')[2]]}>
           {NAV_ITEMS.map((item) => (
-            <Menu.Item key={item} onClick={() => router.push(item)}>
+            <Menu.Item icon={<TeamOutlined />} key={item} onClick={() => router.push(item)}>
               {item.toUpperCase()}
             </Menu.Item>
           ))}
         </Menu>
-      </Header>
-
-      <div style={{ padding: '2rem 4rem' }}>
-        <Space style={{ display: 'flex', alignItems: 'center', marginBottom: 8 }}>
-          <Title level={2} style={{ marginBottom: 0 }}>
-            {pageTitle}
-          </Title>
-          {pageAction}
-        </Space>
-      </div>
-      <Content
-        style={{
-          padding: '0rem 4rem',
-          overflow: 'scroll',
-          ...centerStyle,
-        }}
-      >
-        {children}
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>©{moment().format('YYYY')} Created by pk</Footer>
+      </Sider>
+      <Layout className="site-layout">
+        <PageHeader
+          ghost={false}
+          title="Title"
+          subTitle="This is a subtitle"
+          extra={[
+            <Button key="3">Operation</Button>,
+            <Button key="2">Operation</Button>,
+            <Button key="1" type="primary">
+              Primary
+            </Button>,
+          ]}
+        />
+        <Content style={{ margin: '0 16px' }}>{children}</Content>
+        <Footer style={{ textAlign: 'center' }}>
+          ©{moment().format('YYYY')} Created by parkoon
+        </Footer>
+      </Layout>
     </Layout>
   )
 }
