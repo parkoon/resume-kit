68e80a6aee9b0c4461cad9e685d75a94f7f8293a
parkoon <devparkoon@gmail.com>
2020-12-14 23:49:08 +0900

    feat: EditableTextField 컴포넌트 적용
    issue.
     - 다른 컴포넌트가 udpateMode 일 때 그 이외에 컴포넌트는 udpateMode 가 해지 되어야 하는데!

--- pages/admin/introduce.tsx
+++ pages/admin/introduce.tsx
@@ -1,6 +1,21 @@
+import { Card } from 'antd'
 import AdminLayout from '@Admin/components/AdminLayout'
-import { Card, Button, Form } from 'antd'
-import TextArea from 'antd/lib/input/TextArea'
+import EditableTextField from '@Admin/components/EditableTextField'
+import {
+  FacebookFilled,
+  GithubFilled,
+  HomeFilled,
+  InstagramFilled,
+  LinkedinFilled,
+  MailFilled,
+  MessageFilled,
+  PhoneFilled,
+  SnippetsFilled,
+  ToolFilled,
+  TwitterCircleFilled,
+  UserOutlined,
+  YoutubeFilled,
+} from '@ant-design/icons'
 
 function IntroduceManagement() {
   const onFinish = (values: any) => {
@@ -9,21 +24,88 @@ function IntroduceManagement() {
 
   return (
     <AdminLayout title="내가 누구인가" subtitle="this is subtitle">
-      <Card style={{ width: '50%' }}>
-        <Form initialValues={{ remember: true }} onFinish={onFinish}>
-          <Form.Item
-            name="introduce"
-            rules={[{ required: true, message: 'Please input your introduce!' }]}
-          >
-            <TextArea rows={12} />
-          </Form.Item>
-
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block size="large">
-              SAVE
-            </Button>
-          </Form.Item>
-        </Form>
+      <Card>
+        <EditableTextField
+          icon={<UserOutlined />}
+          placeholder="이름"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<ToolFilled />}
+          placeholder="분야 (ex, 프론트엔드 개발자)"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<MailFilled />}
+          placeholder="이메일"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<GithubFilled />}
+          placeholder="깃허브"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<LinkedinFilled />}
+          placeholder="링크드인"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<FacebookFilled />}
+          placeholder="페이스북"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<YoutubeFilled />}
+          placeholder="유튜브"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<SnippetsFilled />}
+          placeholder="개인블로그"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<PhoneFilled />}
+          placeholder="핸드폰번호"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<InstagramFilled />}
+          placeholder="인스타그램"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<TwitterCircleFilled />}
+          placeholder="트위터"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<HomeFilled />}
+          placeholder="개인홈페이지"
+          initialValue=""
+          onSave={(value) => console.log(value)}
+        />
+        <EditableTextField
+          icon={<MessageFilled />}
+          type="textarea"
+          placeholder="자기소개"
+          initialValue="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione impedit laborum
+          perspiciatis doloribus cum id temporibus, voluptate sint deleniti repellendus, hic, illo
+          iusto quam maxime in eius saepe labore modi."
+          onSave={(value) => console.log(value)}
+        />
       </Card>
     </AdminLayout>
   )
