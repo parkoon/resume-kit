b0f2efa9330bdceaa8ddca35946acf5f2de00996
parkoon <devparkoon@gmail.com>
2020-12-19 21:52:34 +0900

    fix: udpate style

--- src/admin/components/Descriptions/ProjectDescription/index.tsx
+++ src/admin/components/Descriptions/ProjectDescription/index.tsx
@@ -9,12 +9,13 @@ function ProjectDescription({ experience }: ProjectDescriptionProps) {
   return (
     <Card style={{ marginBottom: '1.5rem' }}>
       <Descriptions
-        title={experience.corp}
         bordered
+        title={experience.corp}
         extra={<Button type="dashed">수정하기</Button>}
       >
         <Descriptions.Item label="회사명">{experience.corp}</Descriptions.Item>
         <Descriptions.Item label="프로젝트명">{experience.title}</Descriptions.Item>
+
         <Descriptions.Item label="진행여부">
           {experience.completed ? '종료' : '진행중'}
         </Descriptions.Item>
@@ -22,13 +23,13 @@ function ProjectDescription({ experience }: ProjectDescriptionProps) {
         <Descriptions.Item label="기간">
           {experience.startedAt} ~ {experience.resignedAt}
         </Descriptions.Item>
-        <Descriptions.Item label="기술 스펙" span={4}>
+        <Descriptions.Item label="기술 스펙">
           {experience.skills.map((skill, index) => (
             <Tag key={index}>{skill}</Tag>
           ))}
         </Descriptions.Item>
 
-        <Descriptions.Item label="업무" span={4}>
+        <Descriptions.Item label="업무">
           {experience.tasks.map((task, index) => (
             <div key={index}>{task}</div>
           ))}
