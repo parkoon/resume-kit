d12f5d6dbf01cee56784c6d74d108b436a7ba572
parkoon <devparkoon@gmail.com>
2020-12-20 00:33:02 +0900

    feat: define Profile model (type)

---
+++ src/shared/types/Profile.ts
@@ -0,0 +1,18 @@
+export type ProfileSection =
+  | 'name'
+  | 'position'
+  | 'email'
+  | 'phone'
+  | 'github'
+  | 'linkedin'
+  | 'facebook'
+  | 'twitter'
+  | 'about'
+  | 'homepage'
+  | 'blog'
+  | 'youtube'
+  | 'instagram'
+
+export type Profile = {
+  [key in ProfileSection]: string
+}
