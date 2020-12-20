9090539ffec021f35d2343ee4bdfe3ae0aec245a
parkoon <devparkoon@gmail.com>
2020-12-20 15:14:40 +0900

    feat: 등록된 회사 없을 때 예외처리

--- pages/admin/project.tsx
+++ pages/admin/project.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import { Button, Space, Typography } from 'antd'
+import { useState, useEffect } from 'react'
+import { Button, Space, Typography, Result } from 'antd'
 import Modal from 'antd/lib/modal/Modal'
 import moment from 'moment'
 
 import AdminLayout from '@Admin/layout'
-import { Date, DateString } from '@Admin/components/DatePickers'
 import ProjectForm from '@Admin/components/Forms/ProjectForm'
 import { calcCareerYearAndMonth } from '@Shared/helpers'
 import ProjectDescription from '@Admin/components/Descriptions/ProjectDescription'
 import { Project } from '@Shared/types/Project'
 import ProjectFilter from '@Admin/components/ProjectFilter'
 import EditableDescriptionWithSummary from '@Admin/components/EditableDescriptionWithSummary'
+import useModal from '@Admin/hooks/useModal'
+import { Career } from '@Shared/types/Career'
+import { useRouter } from 'next/router'
 
 const { Title } = Typography
 // 1. 달력으로 일한 기간 선택!
@@ -47,22 +49,11 @@ const projects: Project[] = [
   },
 ]
 
+const careers: Career[] = []
+
 function ExperienceManagement() {
-  const [resigned, setResigned] = useState(true)
-
-  const [isModalVisible, setIsModalVisible] = useState(false)
-
-  const showModal = () => {
-    setIsModalVisible(true)
-  }
-
-  const handleOk = () => {
-    setIsModalVisible(false)
-  }
-
-  const handleCancel = () => {
-    setIsModalVisible(false)
-  }
+  const { visible, open, close } = useModal()
+  const router = useRouter()
 
   const items = [
     {
@@ -83,26 +74,33 @@ function ExperienceManagement() {
     },
   ]
 
-  const [dateString, setDateString] = useState<DateString>('')
-  const [career, setCareer] = useState<{ years: number; months: number }>()
-
-  const handleCareerDateChange = (date: Date, str: DateString) => {
-    setDateString(str)
-
-    if (date) {
-      setCareer(calcCareerYearAndMonth(date))
-    }
+  if (!Boolean(careers.length)) {
+    return (
+      <AdminLayout title="나의 프로젝트" subtitle="지금까지 내가 당담했던 프로젝트를 입력해주세요.">
+        <Result
+          style={{
+            height: '100%',
+            paddingTop: 150,
+          }}
+          status="500"
+          title="프로젝트 등록 오류"
+          subTitle="프로젝트를 등록하기 전에 회사를 등록해주세요"
+          extra={
+            <Button type="primary" onClick={() => router.push('/admin/career')}>
+              등록하기
+            </Button>
+          }
+        />
+      </AdminLayout>
+    )
   }
 
-  const handleSkillChange = (values: string[]) => {
-    console.log(values)
-  }
   return (
     <AdminLayout
-      title="경험"
+      title="나의 프로젝트"
       subtitle="this is subtitle"
       actions={[
-        <Button type="primary" onClick={showModal}>
+        <Button type="primary" onClick={open}>
           만들기
         </Button>,
       ]}
@@ -115,16 +113,12 @@ function ExperienceManagement() {
       />
 
       <EditableDescriptionWithSummary />
-      {/* {projects.map((experience, index) => (
-        <ProjectDescription key={index} experience={experience} />
-      ))} */}
 
-      {/* Project Form Modal */}
       <Modal
         title="경험 만들기"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
+        visible={visible}
+        onOk={close}
+        onCancel={close}
         bodyStyle={{ maxHeight: '70vh', overflow: 'scroll' }}
         destroyOnClose
         footer={[
