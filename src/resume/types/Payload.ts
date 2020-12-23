e2de36bca8366d8836515bf73feee9c9d186841c
parkoon <devparkoon@gmail.com>
2020-12-23 13:23:09 +0900

    feat: 이력서에서 사용 될 타입 정의

---
+++ src/resume/types/Payload.ts
@@ -0,0 +1,17 @@
+import { ArticleJSON } from '@Shared/types/Article'
+import { CareerJSON } from '@Shared/types/Career'
+import { EducationJSON } from '@Shared/types/Education'
+import { EtcJSON } from '@Shared/types/Etc'
+import { ProfileJSON } from '@Shared/types/Profile'
+import { ProjectJSON } from '@Shared/types/Project'
+import { SkillJSON } from '@Shared/types/Skill'
+
+export type Payload = {
+  article: ArticleJSON
+  careers: CareerJSON
+  education: EducationJSON
+  etc: EtcJSON
+  profile: ProfileJSON
+  project: ProjectJSON
+  skill: SkillJSON
+}
