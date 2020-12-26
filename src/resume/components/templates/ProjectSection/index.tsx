545d230732868bb3515faf5d494716f189a9158c
parkoon <devparkoon@gmail.com>
2020-12-26 14:01:14 +0900

    fix: 회사 기준으로 프로젝트 통합
    반복문에 반복문 처리하기 힘들면 컴포넌트를 나눠야 한다는 신호 🧨

--- src/resume/components/templates/ProjectSection/index.tsx
+++ src/resume/components/templates/ProjectSection/index.tsx
@@ -6,54 +6,80 @@ import Space from '@Resume/components/atoms/Space'
 import List from '@Resume/components/atoms/List'
 import { usePayload } from '@Resume/context/PayloadContext'
 import Tag from '@Resume/components/atoms/Tag'
+import { Project } from '@Shared/types/Project'
+
+type ProjectContentProps = {
+  projects: Project[]
+}
+function ProjectContent({ projects }: ProjectContentProps) {
+  return (
+    <>
+      {projects.map(({ id, title, startedAt, endedAt, description, tasks, skills }, index) => (
+        <div key={id} style={{ marginBottom: index === projects.length - 1 ? 0 : 42 }}>
+          <Title level={4}>{title}</Title>
+          <Text italic>
+            {startedAt} ~ {endedAt}
+          </Text>
+
+          <Space top={12}>
+            <Title level={5}>Description</Title>
+            <Text block>{description}</Text>
+          </Space>
+          <Space top={12}>
+            <List title="What I did" items={tasks.map((task) => task.title)} />
+          </Space>
+          <Space top={12}>
+            <Title level={5}>Spec Sheet</Title>
+            <Space top={7}>
+              {skills.map((skill, index) => (
+                <Tag text={skill} />
+              ))}
+            </Space>
+          </Space>
+        </div>
+      ))}
+    </>
+  )
+}
 
 function ProjectSection() {
   const {
     project: { data },
+    career,
   } = usePayload()
+
+  const groupByCorp = data.reduce<{ [key: string]: Project[] }>((prev, project) => {
+    prev[project.where.id] = prev[project.where.id] || []
+    prev[project.where.id].push(project)
+
+    return prev
+  }, {})
+
+  const findCorpById = (id: string) => career.data.find((c) => c.id === id)!
 
   return (
     <Space section>
       <Title level={3} section>
         WORK EXPERIENCE
       </Title>
-      {data.map(({ title, tasks, description, startedAt, skills, endedAt, where }) => (
-        <Description
-          left={
-            <Space>
-              <Title level={4}>{where.title}</Title>
-              <Text block>{where.subtitle}</Text>
-              <Text>
-                {where.startedAt} ~ {where.endedAt}
-              </Text>
-            </Space>
-          }
-          right={
-            <>
-              <Title level={4}>{title}</Title>
-              <Text italic>
-                {startedAt} ~ {endedAt}
-              </Text>
 
-              <Space top={12}>
-                <Title level={5}>Description</Title>
-                <Text block>{description}</Text>
+      {Object.keys(groupByCorp).map((key) => {
+        const corp = findCorpById(key)
+        return (
+          <Description
+            left={
+              <Space>
+                <Title level={4}>{corp.title}</Title>
+                <Text block>{corp.subtitle}</Text>
+                <Text>
+                  {corp.startedAt} ~ {corp.endedAt}
+                </Text>
               </Space>
-              <Space top={12}>
-                <List title="What I did" items={tasks.map((task) => task.title)} />
-              </Space>
-              <Space top={12}>
-                <Title level={5}>Spec Sheet</Title>
-                <Space top={7}>
-                  {skills.map((skill, index) => (
-                    <Tag text={skill} />
-                  ))}
-                </Space>
-              </Space>
-            </>
-          }
-        />
-      ))}
+            }
+            right={<ProjectContent projects={groupByCorp[key]} />}
+          />
+        )
+      })}
     </Space>
   )
 }
