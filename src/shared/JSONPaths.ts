b3fe38a1b46c62b1903c0db7ad2cc2dc5ba77097
parkoon <devparkoon@gmail.com>
2020-12-29 23:33:55 +0900

    Path error in Window prefix `C://`

--- src/shared/JSONPaths.ts
+++ src/shared/JSONPaths.ts
@@ -1,16 +1,14 @@
 import path from 'path'
-
-const DB = path.join(path.resolve(__dirname).split('/server')[0], 'db')
 const JSONPaths = {
-  profile: path.join(DB, 'profile.json'),
-  skills: path.join(DB, 'skills.json'),
-  projects: path.join(DB, 'projects.json'),
-  careers: path.join(DB, 'careers.json'),
-  educations: path.join(DB, 'educations.json'),
-  articles: path.join(DB, 'articles.json'),
-  etcs: path.join(DB, 'etcs.json'),
-  meta: path.join(DB, 'meta.json'),
-  system: path.join(DB, 'system.json'),
+  profile: path.join('db', 'profile.json'),
+  skills: path.join('db', 'skills.json'),
+  projects: path.join('db', 'projects.json'),
+  careers: path.join('db', 'careers.json'),
+  educations: path.join('db', 'educations.json'),
+  articles: path.join('db', 'articles.json'),
+  etcs: path.join('db', 'etcs.json'),
+  meta: path.join('db', 'meta.json'),
+  system: path.join('db', 'system.json'),
 }
 
 export default JSONPaths
