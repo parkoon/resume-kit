2e37c6ffc7480078fe567a06cc2c18f38819ad57
parkoon <devparkoon@gmail.com>
2021-01-09 12:55:48 +0900

    Remove not used code

--- src/admin/types/index.ts
+++ src/admin/types/index.ts
@@ -1,5 +1,3 @@
-import { Moment } from 'moment'
-
 export type FormCompletedType = 'add' | 'modify'
 export type ComponentSize = 'sm' | 'md' | 'lg'
 export type SaveFunc = (name: string, value: string) => void
